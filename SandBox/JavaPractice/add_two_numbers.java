8f28800ad346ff5d8a126abfc45b3d9b126564a9
Taps1 <talatparwez2@gmail.com>
2017-03-13 04:06:22 +0530

    1) Intro to Java 2) Basic Program

---
+++ SandBox/JavaPractice/add_two_numbers.java
@@ -0,0 +1,11 @@
+class Add2Numbers
+{
+    public static void main(String args[])
+    {
+        int num1 = 10;
+        int num2 = 20;
+        int total;
+        total = num1 + num2;
+        System.out.println(total);
+    }
+}
