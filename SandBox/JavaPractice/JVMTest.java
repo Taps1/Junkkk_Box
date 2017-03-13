ad19756f6828df100e461f700a59c015ae6f84c6
Taps1 <talatparwez2@gmail.com>
2017-03-14 01:36:41 +0530

    1. Object creation 2. Dissection of basic program

---
+++ SandBox/JavaPractice/JVMTest.java
@@ -0,0 +1,15 @@
+class Test
+	{
+		public static void main1(String args[])
+		{
+			System.out.println("Hello World!!!");
+		}
+	}
+
+public class JVMTest
+	{
+		public static void main(String args[])
+		{
+			Test.main1(args);
+		}
+	}
