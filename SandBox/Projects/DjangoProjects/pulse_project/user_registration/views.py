e78fa628e7afea396758fa3bcf80ed86cc5ac45e
Taps1 <talatparwez2@gmail.com>
2021-05-22 19:16:54 +0530

    removed extra code

--- SandBox/Projects/DjangoProjects/pulse_project/user_registration/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("\bHello, world. You're at the polls index.\b")
-
