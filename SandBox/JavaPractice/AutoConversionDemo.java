cdd5eced8385fc51efa8dcb86c2e1434d29e65d3
Taps1 <talatparwez2@gmail.com>
2023-07-26 15:36:24 +0530

    removing code files

--- SandBox/JavaPractice/AutoConversionDemo.java
+++
@@ -1,9 +0,0 @@
-public class AutoConversionDemo{
-public static void main(String args[]){
-byte num1 = 10;
-int num2 = num1;
-// byte num3 = num2; Not valid
-byte num3 = (byte)num2;
-System.out.println(num1+" "+num2+" "+num3);
-}
-}
--- SandBox/Python_Practice/self_writing.py
+++
@@ -1,8 +0,0 @@
-def self_writing():
-	f=open("self_writing.py")
-	t=f.read()
-	print t
-	f.close()
-
-if __name__=='__main__':
-	self_writing()
